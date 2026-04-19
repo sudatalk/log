0f8e9ee45bec6df58547909d235fabede8b54f42
ksnd0297 <ksnd0297@gmail.com>
2026-04-19 19:59:28 +0900

    feat(logs): 내 저장 로그를 리뷰 목록에 배치
    MySaveLogCard를 logs 폴더로 옮기고, 로그 페이지 본문에서 저장 로그 섹션을 제거했습니다.
    리뷰 목록 앞에 저장 로그 카드를 먼저 노출하도록 배치 순서를 조정했습니다.

--- src/app/logs/[id]/page.tsx
+++ src/app/logs/[id]/page.tsx
@@ -3,7 +3,6 @@ import { Header } from "@/components/Header";
 import { BG_BASE, FLEX, FLEX_COL, FULL } from "@/constants/tailwind";
 import clsx from "clsx";
 import LogsContainer from "./components/LogsContainer";
-import MySaveLogs from "./components/MySaveLogs";
 import Logs from "./components/Logs";
 
 export default async function LogsPage() {
@@ -11,7 +10,6 @@ export default async function LogsPage() {
     <div className={clsx(FULL, FLEX, BG_BASE, FLEX_COL)}>
       <Header />
       <LogsContainer>
-        <MySaveLogs />
         <Logs />
       </LogsContainer>
       <BottomNav />
