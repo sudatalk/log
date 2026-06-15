762c329d47227963fe8e1731c5af7bc25c44cebb
ksnd0297 <ksnd0297@gmail.com>
2026-06-15 21:06:51 +0900

    feat : 리뷰 작성 및 수정 페이지 컴포넌트 제작

---
+++ src/app/write/[id]/page.tsx
@@ -0,0 +1,5 @@
+const ReviewModifyPage = () => {
+  return <>리뷰 수정 페이지</>;
+};
+
+export default ReviewModifyPage;
---
+++ src/app/write/page.tsx
@@ -0,0 +1,5 @@
+const ReviewWritePage = () => {
+  return <>리뷰 작성 페이지</>;
+};
+
+export default ReviewWritePage;
