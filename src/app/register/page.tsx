0c8a0a46753a0a211c2589d203a02c78b2e49878
ksnd0297 <ksnd0297@gmail.com>
2026-07-07 22:24:29 +0900

    feat : 회원가입 프로필 이미지 제작

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -9,6 +9,7 @@ import {
   FLEX,
   FLEX_1,
   FLEX_COL,
+  GAP_5,
   H_FULL,
   JUSTIFY_BETWEEN,
   MIN_H_DVH,
@@ -22,6 +23,14 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { REDIRECT_URL_KEY } from "@/constants/router";
 import axios from "axios";
 
+const profiles = [
+  "/profile/profile-1.png",
+  "/profile/profile-2.png",
+  "/profile/profile-3.png",
+  "/profile/profile-4.png",
+  "/profile/profile-5.png",
+];
+
 const RegisterPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -33,6 +42,8 @@ const RegisterPage = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const [nickname, setNickname] = useState("");
+
+  const [selected, setSelected] = useState(0);
 
   const nicknameId = useId();
 
@@ -78,21 +89,48 @@ const RegisterPage = () => {
   return (
     <>
       <div className={clsx(W_FULL, MIN_H_DVH, BG_BASE, FLEX, FLEX_COL, "p-4")}>
-        <div className={clsx(FLEX, FLEX_COL, JUSTIFY_BETWEEN, H_FULL, FLEX_1)}>
-          <Field>
-            <FieldLabel htmlFor={nicknameId}>
-              닉네임<span className="text-destructive">*</span>
-            </FieldLabel>
-            <Input
-              id={nicknameId}
-              type="text"
-              value={nickname}
-              onChange={(event) => setNickname(event.target.value)}
-              placeholder="닉네임을 입력해주세요"
-              style={{ backgroundColor: "white", height: 40 }}
-            />
-            <FieldDescription>앱 내에서 사용할 닉네임을 알려주세요</FieldDescription>
-          </Field>
+        <div className={clsx(FLEX, FLEX_COL, JUSTIFY_BETWEEN, H_FULL)}>
+          <div className={clsx(FLEX, FLEX_COL, H_FULL, FLEX_1, GAP_5)}>
+            <Field>
+              <FieldLabel htmlFor={nicknameId}>
+                닉네임<span className="text-destructive">*</span>
+              </FieldLabel>
+              <Input
+                id={nicknameId}
+                type="text"
+                value={nickname}
+                onChange={(event) => setNickname(event.target.value)}
+                placeholder="닉네임을 입력해주세요"
+                style={{ backgroundColor: "white", height: 40 }}
+              />
+              <FieldDescription>앱 내에서 사용할 닉네임을 알려주세요</FieldDescription>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor={nicknameId}>
+                프로필 사진<span className="text-destructive">*</span>
+              </FieldLabel>
+              <div className="grid grid-cols-5 gap-4 mb-2">
+                {profiles.map((src, index) => (
+                  <div
+                    key={src}
+                    onClick={() => setSelected(index)}
+                    className={clsx(
+                      "size-18 rounded-full overflow-hidden cursor-pointer transition-all duration-200 flex items-center justify-center bg-gray-100",
+                      selected === index
+                        ? "ring-2 ring-blue-500 ring-offset-2 scale-105"
+                        : "hover:ring-2 hover:ring-gray-300"
+                    )}
+                  >
+                    <img
+                      src={src}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                ))}
+              </div>
+              <FieldDescription>앱 내에서 사용할 프로필 사진을 알려주세요</FieldDescription>
+            </Field>
+          </div>
           <Button
             type="submit"
             size="lg"
