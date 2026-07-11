28a731a2bb219fc659aaea472abea16e8ad9a337
ksnd0297 <ksnd0297@gmail.com>
2026-07-11 16:20:57 +0900

    feat : BooksContainer over-y-auto 추가

--- src/app/books/components/BooksContainer.tsx
+++ src/app/books/components/BooksContainer.tsx
@@ -2,7 +2,7 @@ import { FLEX, FLEX_1, FLEX_COL, GAP_3, H_FULL, P_3, W_FULL } from "@/constants/
 import clsx from "clsx";
 
 const BooksContainer = ({ children }: { children: React.ReactNode }) => {
-  return <div className={clsx(FLEX, FLEX_COL, W_FULL, H_FULL, P_3, GAP_3, FLEX_1)}>{children}</div>;
+  return <div className={clsx(FLEX, FLEX_COL, W_FULL, H_FULL, P_3, GAP_3, FLEX_1, "overflow-y-auto")}>{children}</div>;
 };
 
 export default BooksContainer;
