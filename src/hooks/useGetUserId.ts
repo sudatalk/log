c06ddfe0bc15cf8f47acc22a2040e94fbc6daec8
ksnd0297 <ksnd0297@gmail.com>
2026-07-19 21:59:47 +0900

    fix : ensureQueryData -> fetchQuery로 변경 - 캐시 사용 X

--- src/hooks/useGetUserId.ts
+++ src/hooks/useGetUserId.ts
@@ -4,43 +4,47 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const useGetUserId = () => {
-    const [userId, setUserId] = useState<number | undefined>();
-    const [isLoading, setIsLoading] = useState(true);
+  const [userId, setUserId] = useState<number | undefined>();
+  const [isLoading, setIsLoading] = useState(true);
 
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const statusInfo = await Kakao.Auth.getStatusInfo();
+  useEffect(() => {
+    (async () => {
+      try {
+        const statusInfo = await Kakao.Auth.getStatusInfo();
 
-                if ("error" in statusInfo) {
-                    throw new Error("Kakao Auth Error");
-                }
+        if ("error" in statusInfo) {
+          throw new Error("Kakao Auth Error");
+        }
 
-                const { status, user } = statusInfo;
+        const { status, user } = statusInfo;
 
-                if (status === "connected" && !!user) {
-                    const { id: appUserId } = user;
+        if (status === "connected" && !!user) {
+          const { id: appUserId } = user;
 
-                    const userResponse = await queryClient.ensureQueryData({
-                        queryKey: ["CHECK_USER"],
-                        queryFn: () => getCheckUser({ appUserId: +appUserId })
-                    })
+          const userResponse = await queryClient.fetchQuery({
+            queryKey: ["CHECK_USER"],
+            queryFn: () => getCheckUser({ appUserId: +appUserId }),
+          });
 
-                    if (!userResponse.registered || userResponse.status === UserStatus.WITHDRAW) return;
+          if (
+            !userResponse.registered ||
+            userResponse.status === UserStatus.WITHDRAW
+          )
+            return;
 
-                    setUserId(userResponse.userId);
-                }
-            } catch {
-                return;
-            } finally {
-                setIsLoading(false);
-            }
-        })();
-    }, [])
+          setUserId(userResponse.userId);
+        }
+      } catch {
+        return;
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
 
-    return { userId, isLoading }
-}
+  return { userId, isLoading };
+};
 
 export default useGetUserId;
