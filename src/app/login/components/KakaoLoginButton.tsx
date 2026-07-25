f7d0f19caecc3780192cfa73a1b0141a057f4ef0
ksnd0297 <ksnd0297@gmail.com>
2026-07-25 18:32:37 +0900

    feat : Authorize -> REST 방식으로 변경

--- src/app/login/components/KakaoLoginButton.tsx
+++ src/app/login/components/KakaoLoginButton.tsx
@@ -7,19 +7,30 @@ type Props = {
 };
 
 const KakaoLoginButton = (props: Props) => {
+
   const { redirectUrl = "/" } = props;
 
   const handleClickKakaoLoginButton = () => {
-    Kakao.Auth.authorize({
-      redirectUri: NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI,
-      ...(redirectUrl ? { state: redirectUrl } : {}),
-    });
+    const url = new URL("https://kauth.kakao.com/oauth/authorize");
+
+    url.searchParams.set("client_id", "ae45be10073c4af64e587798d03ebc85");
+    url.searchParams.set("redirect_uri", `${NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI}`);
+    url.searchParams.set('state', `${redirectUrl}`);
+    url.searchParams.set("response_type", "code");
+
+    window.location.href = url.toString();
   };
 
   return (
     <>
       <Button onClick={handleClickKakaoLoginButton}>
-        <Image src="/kakao_login_medium_wide.png" alt="카카오 로그인" width={300} height={45} priority />
+        <Image
+          src="/kakao_login_medium_wide.png"
+          alt="카카오 로그인"
+          width={300}
+          height={45}
+          priority
+        />
       </Button>
     </>
   );
