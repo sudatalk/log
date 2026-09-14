4b29eb41120d4cb4df1f7ba71c68d1fa969fa5e6
ksnd0297 <ksnd0297@gmail.com>
2026-09-14 22:55:34 +0900

    feat : access_token 로컬스토리지 저장 로직 추가

--- src/components/CheckHeader.tsx
+++ src/components/CheckHeader.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import {
-  fetchCurrentUserId,
-  USER_ID_QUERY_KEY,
-} from "@/hooks/useGetUserId";
+import { fetchCurrentUserId, USER_ID_QUERY_KEY } from "@/hooks/useGetUserId";
+import useLocalStorage from "@/hooks/useLocalStorage";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const CheckHeader = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(true);
   const queryClient = useQueryClient();
+
+  const [access_token, setAccessToken] = useLocalStorage<string>("access_token", "");
+
+  useEffect(() => {
+    if (!access_token) return;
+    try {
+      Kakao.Auth.setAccessToken(access_token);
+    } catch {
+      setAccessToken("");
+    }
+  }, [access_token]);
 
   useEffect(() => {
     (async () => {
